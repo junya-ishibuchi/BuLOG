f5fc6b8cab9b54109e4f8def9555dc55b3474cdf
Junya Ishibuchi <junya.ishibuchi@gmail.com>
2021-07-09 15:34:31 -0700

    change favicon

--- src/html.js
+++ src/html.js
@@ -12,6 +12,15 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🍜</text></svg>"
+        />
+        <link
+          rel="icon alternate"
+          type="image/png"
+          href="https://twemoji.maxcdn.com/v/13.0.2/72x72/1f638.png"
+        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
